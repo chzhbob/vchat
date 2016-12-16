0a9134ce5222e59d92bcc5e23062d2c18e07ac29
chzhbob <chzhbob@gmail.com>
2016-12-16 17:51:12 +0800

    login page

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App'
+import '../static/core.css'
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+	el: '#app',
+	template: '<App/>',
+	components: { App }
 })
