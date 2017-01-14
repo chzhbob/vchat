8364016965f8be901c46218131f8a3b9168f2571
chzhbob <chzhbob@gmail.com>
2017-01-14 23:12:32 +0800

    vue router

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,26 @@
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
+
+import Main from './Main.vue'
+import App from './App.vue'
+
+
 import '../static/core.css'
 
+Vue.use(VueRouter);
+
+const routes = [
+	{ path: '/admin', component: App }
+]
+
+const router = new VueRouter({
+	routes
+})
+
+
 new Vue({
-	el: '#app',
-	template: '<App/>',
-	components: { App }
+  el: '#app',
+  router: router,
+  template: '<Main/>',
+  components: { Main }
 })
