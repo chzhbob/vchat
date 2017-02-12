542d649cb468d8dbd3869cb2f778ed1f1ae3d92f
chzhbob <chzhbob@gmail.com>
2017-02-12 22:12:09 +0800

    homepage

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -5,21 +5,67 @@
 		</section>
 		<section class="features-wrapper">
 			<section class="features">
+				<section class="main-logo">
+					<section class="line"></section>
+					<img src="~assets/logo.png" />
+					<section class="line"></section>
+				</section>
+
 				<h1><i>We Provide Training & Tools For <br/> Assessing Executive Function</i></h1>
 				<section class="features-detail">
 					<p>幼儿园的工作是复杂而具体的。光幼儿园的一日生活就包括：专门的教育教学活动、游戏活动还有生活常规活动等多种多样的活动。其中的生活常规活动又包括：用餐、就寝、入厕等很多小的细节和工作。这些活动虽然微不足道，也不被大家所重视，但却是幼儿养成良好生活习惯和幼儿园一日活动得以顺利开展中不可缺少的重要环节。</p>
 					<p>幼儿的一日生活常规规范着幼儿的言行，它体现在幼儿的生活习惯、学习习惯和行为习惯上。因此在班级常规管理过程中，我们班两位老师总是目标一致，相互配合，统一要求;对孩子的要求坚持一贯，不朝令夕改。</p>
 				</section>
-				<section class="feature-block">
+
+				<section class="feature-block one">
 					<section class="feature-bg"></section>
 					<section class="feature-cover">
 						<img src="~assets/cover1.jpg" />
 					</section>
 					<section class="feature-detail">
-						<h2>对幼儿做到要求统一</h2>
+						<h2><i>对幼儿做到<br />要求统一</i></h2>
 						<p>在一个班集体中，两位教师由于性格、生活环境等不同，工作作风、工作方法也不尽相同，但不管怎样在引导孩子明辨是非，培养良好的行为习惯等方面，达成共识，做到要求统一。就这样在统一要求，严格管理中，孩子们逐渐明白了该怎样做，不该怎样做。</p>
-						<a href="javascript:void(0)">更多内容</a>
+						<a class="more" href="javascript:void(0)">更多内容 &gt;</a>
 					</section>
+				</section>
+
+				<section class="feature-block two">
+					<section class="feature-bg"></section>
+					<section class="feature-detail">
+						<h2><i>对幼儿做到<br />要求统一</i></h2>
+						<p>在一个班集体中，两位教师由于性格、生活环境等不同，工作作风、工作方法也不尽相同，但不管怎样在引导孩子明辨是非，培养良好的行为习惯等方面，达成共识，做到要求统一。就这样在统一要求，严格管理中，孩子们逐渐明白了该怎样做，不该怎样做。</p>
+						<a class="more" href="javascript:void(0)">更多内容 &gt;</a>
+					</section>
+					<section class="feature-cover">
+						<img src="~assets/cover2.jpg" />
+					</section>
+				</section>
+
+				<section class="feature-block three">
+					<section class="feature-bg"></section>
+					<section class="feature-cover">
+						<img src="~assets/cover3.jpg" />
+					</section>
+					<section class="feature-detail">
+						<h2><i>对幼儿做到<br />要求统一</i></h2>
+						<p>在一个班集体中，两位教师由于性格、生活环境等不同，工作作风、工作方法也不尽相同，但不管怎样在引导孩子明辨是非，培养良好的行为习惯等方面，达成共识，做到要求统一。就这样在统一要求，严格管理中，孩子们逐渐明白了该怎样做，不该怎样做。</p>
+						<a class="more" href="javascript:void(0)">更多内容 &gt;</a>
+					</section>
+				</section>
+			</section>
+			<section class="features-belt">
+			</section>
+		</section>
+
+		<section class="demo-link-wrapper">
+			<section class="demo-link">
+				<section class="demo-desc">
+					<h2>这些幼儿园的实践<br />你跟上了吗？</h2>
+					<p>幼儿园的工作是复杂而具体的。光幼儿园的一日生活就包括：专门的教育教学活动、游戏活动还有生活常规活动等多种多样的活动。其中的生活常规活动又包括：用餐、就寝、入厕等很多小的细节和工作。这些活动虽然微不足道，也不被大家所重视，但却是幼儿养成良好生活习惯和幼儿园一日活动得以顺利开展中不可缺少的重要环节。</p>
+					<a href="javascript:void(0)">立即体检</a>
+				</section>
+				<section class="demo-cover">
+					<img src="~assets/cover4.jpg">
 				</section>
 			</section>
 		</section>
@@ -40,10 +86,30 @@
 .features-wrapper{
 	background: #299ee1;
 	color: white;
+	position: relative;
+	overflow: hidden;
 }
 .features{
 	width: 1100px;
 	margin: 0 auto;
+}
+.main-logo{
+	text-align: center;
+	padding: 40px 0 10px;
+}
+.main-logo img{
+	opacity: 0.2;
+	height: 62px;
+	width: 62px;
+}
+.main-logo .line{
+	height: 3px;
+	width: 100px;
+	background: #2994da;
+	display: inline-block;
+	position: relative;
+	top: -28px;
+	margin: 0 25px;
 }
 .features h1{
 	font-size: 34px;
@@ -52,7 +118,7 @@
 	padding: 30px 0;
 }
 .features-detail{
-	padding: 10px 0;
+	padding: 10px 90px;
 	overflow: hidden;
 }
 .features-detail p{
@@ -60,7 +126,7 @@
 	width: 50%;
 	padding: 0 30px;
 	font-size: 16px;
-	line-height: 24px;
+	line-height: 28px;
 }
 .features-detail p:nth-child(odd){
 	border-right: 1px solid #2994da;
@@ -72,16 +138,105 @@
 .feature-block{
 	position: relative;
 	width: 1050px;
+	margin: 50px 0 190px;
+	left: 50px;
+	z-index: 1;
 }
 .feature-block .feature-bg{
 	position: absolute;
 	height: 445px;
 	width: 100%;
-	left: -50px;
 	top: 30px;
-	background: #f59a16;
+	box-shadow: 0px 15px 50px rgba(0,0,0,0.2);
 }
-.feature-block{
-	
+.feature-block .feature-cover{
+	width: 535px;
+	height: 100%;
+	float: left;
+	position: relative;
+	box-shadow: 20px 0px 40px rgba(0,0,0,0.2);
+	z-index: 1;
+}
+.feature-block .feature-cover img{
+	width: 100%;
+	height: 100%;
+	object-fit:cover;
+	display: block;
+}
+.feature-block .feature-detail{
+	margin-left: 535px;
+	position: relative;
+	background: white;
+	top: 30px;
+	padding: 50px 45px;
+	box-shadow: 0px 15px 50px rgba(0,0,0,0.2);
+}
+.feature-block .feature-detail h2{
+	color: #273b92;
+	font-size: 26px;
+	font-weight: bold;
+}
+.feature-block .feature-detail p{
+	color: #818183;
+	font-size: 16px;
+	padding: 20px 0;
+	line-height: 24px;
+}
+.feature-block .feature-detail .more{
+	font-size: 18px;
+	color: #283c93;
+	font-weight: bold;
+	text-decoration: none;
+}
+
+.feature-block.one{
+	left: 50px;
+}
+.feature-block.two{
+	left: 0px;
+	margin-bottom: 150px;
+}
+.feature-block.three{
+	left: 50px;
+}
+.feature-block.one .feature-bg{
+	background: #f59a16;
+	left: -50px;
+}
+.feature-block.two .feature-bg{
+	background-color: #aa5590;
+	right: -50px;
+}
+.feature-block.three .feature-bg{
+	background-color: #41c6b3;
+	left: -50px;
+}
+.feature-block.two .feature-cover{
+	margin-left: 510px;
+	float: none;
+	box-shadow: -20px 0px 40px rgba(0,0,0,0.2);
+}
+.feature-block.two .feature-detail{
+	width: 510px;
+	float: left;
+	margin: 0px;
+}
+.features-belt{
+	width: 100%;
+	height: 980px;
+	background: #2693d4;
+	position: absolute;
+	bottom: 13%;
+	left: 0;
+	transform: skewY(-45deg);
+	z-index: 0;
+}
+
+
+.demo-link{
+	height: 460px;
+	width: 100%;
+	background: url(~assets/wood.jpg) repeat-x;
+	background-size: cover;
 }
 </style>
